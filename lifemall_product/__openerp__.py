ff837774b3547e4dc08fedf1a7bbfc0c951adb03
zuoqin <zuoqinr@163.com>
2015-01-11 21:03:19 +0800

    Initial customisation

--- lifemall_product/__openerp__.py
+++ lifemall_product/__openerp__.py
@@ -26,46 +26,11 @@
  'license': 'AGPL-3',
  'category': 'Generic Modules',
  'description': """
-Connector
+Customisation for Lifemall Backend
 =========
 
-This is a framework designed to build connectors with external systems,
-usually called `Backends`.
-
-Documentation: http://openerp-connector.com
-
-It features:
-
-* A jobs queue
-
-    In which the connectors can push functions (synchronization tasks)
-    to be executed later.
-
-* An event pattern
-
-    The connectors can subscribe consumer methods, executed when the events
-    are fired.
-
-* Connector base classes
-
-    Called ``ConnectorUnit``.
-
-    Include base classes for the use in connectors, ready to be extended:
-
-    * ``Synchronizer``: flow of an import or export
-    * ``Mapper``: transform a record according to mapping rules
-    * ``Binder``: link external IDs with local IDS
-    * ``BackendAdapter``: adapter interface for the exchanges with the backend
-
-* A multi-backend support
-
-    Each ``ConnectorUnit`` can be registered amongst a backend type (eg.
-    Magento) or a backend version only.
-
-It is actually used to connect Magento_ and Prestashop_
-
-.. _Magento: http://openerp-magento-connector.com
-.. _Prestashop: https://launchpad.net/prestashoperpconnect
+This application designed and supported by
+ZuoQin, zuoqinr@163.com
 """,
  'depends': ['sale',
              'purchase',
